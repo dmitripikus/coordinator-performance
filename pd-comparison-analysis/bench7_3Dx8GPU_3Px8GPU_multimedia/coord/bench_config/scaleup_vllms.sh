5d56d16a5fdc7479a0dac7a0bc0f9a95be69a696
Dmitri Pikus <DPIKUS@il.ibm.com>
2026-07-14 18:37:09 +0300

    Benchmark configuration update and test results for 3Dx8GPU_3Px8GPU multimedia test with coordinator (bench7_3Dx8GPU_3Px8GPU_multimedia)
    Signed-off-by: Dmitri Pikus <DPIKUS@il.ibm.com>

--- pd-comparison-analysis/bench7_3Dx8GPU_3Px8GPU_multimedia/coord/bench_config/scaleup_vllms.sh
+++ pd-comparison-analysis/bench7_3Dx8GPU_3Px8GPU_multimedia/coord/bench_config/scaleup_vllms.sh
@@ -1,2 +1,2 @@
-k scale deploy epd-nvidia-gpu-vllm-decode --replicas=3
-k scale deploy epd-nvidia-gpu-vllm-prefill --replicas=3
+kubectl scale deploy epd-nvidia-gpu-vllm-decode --replicas=3
+kubectl scale deploy epd-nvidia-gpu-vllm-prefill --replicas=3
